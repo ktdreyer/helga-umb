6ec35c7300b90d11182cd11826db5269b1ace41f
Ken Dreyer <kdreyer@redhat.com>
2017-12-08 20:05:27 -0700

    qe.ci.centos: image name and type headers have underscores
    We were looking for "image-name" and "image-type" headers, and it's
    really "image_name" and "image_type".
    
    We can now drop the debugging code here.

--- helga_umb/topics/qe/ci/centos.py
+++ helga_umb/topics/qe/ci/centos.py
@@ -6,16 +6,7 @@ def consume(client, channel, frame):
     message = 'centos %s' % msg_type
     if msg_type == 'centos-image':
         # image_name eg. "CentOS-Atomic-Host-7.1711-Vagrant-VirtualBox.box"
-        image_name = frame.headers.get('image-name')
-        if image_name is None:
-            # Not sure why some messages lack image-name. Let's get more
-            # information when we hit this.
-            from pprint import pprint
-            pprint(frame.headers)
-            try:
-                pprint(frame.body.decode())
-            except UnicodeDecodeError:
-                pass
-        image_type = frame.headers['image-type']  # "atomic"
+        image_name = frame.headers['image_name']
+        image_type = frame.headers['image_type']  # "atomic"
         message = 'centos published %s image %s' % (image_type, image_name)
     client.msg(channel, message)
