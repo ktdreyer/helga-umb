cf31c2aa0f9cf9b9602b9eebd015d2dcec5ad2dd
Ken Dreyer <kdreyer@redhat.com>
2018-08-01 14:04:19 -0600

    ignore VirtualTopic.qe.ci.centos.messages

--- helga_umb/__init__.py
+++ helga_umb/__init__.py
@@ -74,6 +74,7 @@ class HelgaStompConsumer():
             '/topic/VirtualTopic.eng.rpmdiff',
             '/topic/VirtualTopic.qe.ci.brew',  # Mirrors everything from brew
             '/topic/VirtualTopic.qe.ci.brew-build',  # some kinda "tests"
+            '/topic/VirtualTopic.qe.ci.centos.messages',  # just heartbeats
             '/topic/VirtualTopic.qe.ci.create-task-repo',
             '/topic/VirtualTopic.qe.ci.distgit',
             '/topic/VirtualTopic.qe.ci.errata',
