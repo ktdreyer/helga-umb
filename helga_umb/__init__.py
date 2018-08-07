f0c6858745d90205e74eec0eb5ebaafa655b2336
Ken Dreyer <kdreyer@redhat.com>
2018-08-07 15:34:21 -0600

    rm "with failover" comment
    all URLs use failover now

--- helga_umb/__init__.py
+++ helga_umb/__init__.py
@@ -16,10 +16,10 @@ from stompest.async.listener import SubscriptionListener
 
 from helga_umb import datagrepper
 
-# prod with failover
+# prod
 STOMP_URL = 'failover:(ssl://messaging-devops-broker01.web.prod.ext.phx2.redhat.com:61612,ssl://messaging-devops-broker02.web.prod.ext.phx2.redhat.com:61612)?startupMaxReconnectAttempts=10'  # NOQA: E501
 
-# stage with failover
+# stage
 # STOMP_URL = 'failover:(ssl://messaging-devops-broker01.web.stage.ext.phx2.redhat.com:61612,ssl://messaging-devops-broker02.web.stage.ext.phx2.redhat.com:61612)?startupMaxReconnectAttempts=10'  # NOQA: E501
 
 # dev
